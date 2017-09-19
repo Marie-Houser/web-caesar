5ccfd7ff2bf4867461ae98a4d092737297c72657
Marie Houser <joe.and.marie.houser@gmail.com>
2017-09-18 22:05:21 -0500

    NOW I've finished the bloody thing.

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ form = """
 <html>
     <head>
         <style>
-            form{
+            form{{
                 background-color: #eee
                 padding: 20px;
                 margin 0 auto;
                 width 540px;
                 font: 16px sans-serif
                 border-radius: 10px;
-             }
-             textarea{
+             }}
+             textarea{{
                  margin: 10px 0;
                  width: 540px;
                  height: 120px;
-             }
+             }}
         </style>
     </head>
     <body>
     <form method="post">
         Rotate by:<input type="text" name="rot" value="0">
-        <input type="textarea" name="text">
+        <textarea name="text">{0}</textarea>
         <input type="submit" value="Submit">
     </form>
     </body>
@@ -35,12 +35,12 @@ form = """
 """
 @app.route("/")
 def index ():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt ():
     rot = int(request.form['rot'])
     text= request.form['text']
-    return '<h1>'+rotate_string(text, rot)+'</h1>'
+    return form.format(rotate_string(text, rot))
 
 app.run()
